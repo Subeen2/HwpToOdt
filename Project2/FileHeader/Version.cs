1985521b1195e0e831330f3a452e2ab5c6d5e62b
201402333@o.cnu.ac.kr <201402333@o.cnu.ac.kr>
2021-05-22 17:50:18 +0900

    1차

---
+++ Project2/FileHeader/Version.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using java.lang;
+using Newtonsoft.Json.Linq;
+
+namespace FileHeader
+{
+    public class Version : Type
+    {
+        internal JObject level3 = new JObject();
+
+        public Version() : base(DWORD)
+        {
+            // TODO Auto-generated con	structor stub
+        }
+
+
+        public virtual string print()
+        {
+            string str1 = "";
+            Stack stk = new Stack();
+            for (int i = 0; i < Array_Conflict.Length; i++)
+                stk.Push(Array_Conflict[i]);
+            while (stk.Count != 0)
+            {
+                str1 = str1 + stk.Pop() + "";
+                if (stk.Count != 0)
+                    str1 += ".";
+            }
+            return str1;
+        }
+
+        public virtual void toJSON()
+        {
+
+            level3.Add("Version", print());
+
+
+        }
+
+    }
+}
